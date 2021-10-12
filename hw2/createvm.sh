584eac0961a4ee906822d729c18a7301a9433b63
LogCreative <lizilong123456@live.cn>
2021-10-12 09:39:30 +0800

    hw2 vnc

--- hw2/createvm.sh
+++ hw2/createvm.sh
@@ -5,9 +5,8 @@ sudo -s
 # install vm
 qemu-img create -f qcow2 ubuntu.img 10G
 # cd ~/Documents/virtualization
-# wget https://mirror.sjtu.edu.cn/ubuntu-cd/18.04.6/ubuntu-18.04.6-live-server-amd64.iso
-# qemu-system-x86_64 -m 2048 -nographic ubuntu.img -cdrom ./ubuntu-18.04.6-live-server-amd64.iso
-qemu-system-x86_64 -m 2048 -enable-kvm ubuntu.img -cdrom ./ubuntu-18.04.5-desktop-amd64.iso
+wget https://mirror.sjtu.edu.cn/ubuntu-cd/18.04.6/ubuntu-18.04.6-desktop-amd64.iso
+qemu-system-x86_64 -m 2048 -enable-kvm ubuntu.img -cdrom ./ubuntu-18.04.6-desktop-amd64.iso
 
 ##  INSIDE VM 
 ##  sudo dpkg-reconfigure unattended-upgrades
--- hw2/vnc.sh
+++ hw2/vnc.sh
@@ -1,11 +1,4 @@
 # !/bin/bash
 
-cd /home
-curl -O https://www.realvnc.com/download/file/vnc.files/VNC-Server-6.7.4-Linux-x64.deb
-apt install ./VNC-Server-6.7.4-Linux-x64.deb
-curl -O https://www.realvnc.com/download/file/viewer.files/VNC-Viewer-6.21.406-Linux-x64.deb
-apt install VNC-Viewer-6.21.406-Linux-x64.deb
-# vnclicense -add *****-*****-*****-*****-*****
-apt-get install gnome-core
-
-vncserver
+apt install xfce4 xfce4-goodies vnc4server
+vncserver -geometry 1280x780
